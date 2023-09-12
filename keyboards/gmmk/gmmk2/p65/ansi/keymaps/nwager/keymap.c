fbe427617a1e6301de88368289c448e77acaa786
Noah Wager <noahwager@gmail.com>
2023-09-11 17:37:02 -0700

    enable caps lock led and rename stuff

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/nwager/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/nwager/keymap.c
@@ -20,36 +20,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 
 enum layers {
-    BASE,  // default, swap GESC and GRV for programming
-    GAME,  // gaming layer, put back GESC since menus use ESC a lot
-    FN,    // fn layer
+    L_BASE,  // default, swap GESC and GRV for programming
+    L_GAME,  // gaming layer, put back GESC since menus use ESC a lot
+    L_FN,    // fn layer
+    L_NUM_,  // use this for number of layers, not an actual layer
 };
 
 // Update matrix for the current state.
 void rgb_matrix_state_update(uint32_t state);
 
-#define KC_FN MO(FN)
-#define FN_LED_IDX 63
+#define KC_FN MO(L_FN)
+#define FN_LED 63
+
+#define CAPS_LED 30
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = LAYOUT(
+[L_BASE] = LAYOUT(
    KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_PGUP,
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGDN,
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_END,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_FN,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-[GAME] = LAYOUT(
+[L_GAME] = LAYOUT(
   KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_PGUP,
   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGDN,
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_END,
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_FN,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-[FN] = LAYOUT(
+[L_FN] = LAYOUT(
   KC_GESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  KC_INS,
   _______,   KC_GRV,  _______,  DB_TOGG,  QK_BOOT,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
-  _______,  _______,  _______,  _______,  _______, TG(GAME),  _______,  _______,  _______,  _______,  _______,  _______,             _______,
+  _______,  _______,  _______,  _______,  _______, TG(L_GAME),  _______,  _______,  _______,  _______,  _______,  _______,             _______,
   _______,  _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,   RGB_VAI,  KC_HOME,
   _______,  _______,  _______,                                RESET,                                  _______,  _______,  RGB_RMOD,  RGB_VAD,  RGB_MOD),
 };
@@ -65,15 +68,19 @@ uint32_t layer_state_set_user(uint32_t state) {
 }
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    // light up FN key
-    if (IS_LAYER_ON_STATE(layer_state, FN)) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(FN_LED_IDX, 255, 255, 255);
+    // light up L_FN key when active
+    if (IS_LAYER_ON_STATE(layer_state, L_FN)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(FN_LED, 255, 255, 255);
+    }
+    // light up caps lock when active
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LED, 255, 255, 255);
     }
 }
 
 void rgb_matrix_state_update(uint32_t state) {
     // NOTE: base layer is always on
-    if (IS_LAYER_ON_STATE(state, GAME)) {
+    if (IS_LAYER_ON_STATE(state, L_GAME)) {
 	rgb_matrix_sethsv(0, 255, 255);
         rgb_matrix_mode(RGB_MATRIX_CYCLE_OUT_IN);
         rgb_matrix_set_speed(48);
