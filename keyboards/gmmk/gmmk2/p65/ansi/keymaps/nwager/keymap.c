23e0a19b4e95c17dbbe5b16e900a7b337f73599b
Noah Wager <noahwager@gmail.com>
2023-09-16 12:46:19 -0700

    Add per-layer RGB state
    - allows each layer to track and modify own RGB settings
    - disable RGB keycodes and add custom ones
    
    Signed-off-by: Noah Wager <noahwager@gmail.com>

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/nwager/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/nwager/keymap.c
@@ -15,6 +15,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "utils.h"
+
+#define LRGB_VA_STEP 26
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -26,8 +29,47 @@ enum layers {
     L_NUM_,  // use this for number of layers, not an actual layer
 };
 
+enum custom_keycodes {
+    KC_LRGB_TOG = SAFE_RANGE,
+    KC_LRGB_RST,
+    KC_LRGB_VAI,
+    KC_LRGB_VAD,
+};
+
+struct rgb_state {
+    bool on;
+    uint8_t h, s, v, spd;
+    int mode;
+};
+
+static struct rgb_state PROGMEM LRGB_DEFAULT[L_NUM_] = {
+    [L_BASE] = {
+        .on   = true,
+        .spd  = 14,
+        .s    = 255,
+        .v    = 130,
+        .mode = RGB_MATRIX_CYCLE_ALL,
+    },
+    [L_GAME] = {
+        .on   = true,
+        .spd  = 48,
+        .s    = 255,
+        .v    = 160,
+        .mode = RGB_MATRIX_CYCLE_OUT_IN,
+    },
+    [L_FN] = {
+        .on   = true,
+        .mode = RGB_MATRIX_NONE,
+    }
+};
+static struct rgb_state lrgb_state[L_NUM_];
+
 // Update matrix for the current state.
-void rgb_matrix_state_update(uint32_t state);
+static void rgb_matrix_refresh(uint32_t state);
+// Get highest non-NONE active layer.
+static uint8_t get_highest_rgb_layer(uint32_t state);
+// Reset RGB settings to default.
+static void lrgb_reset(void);
 
 #define KC_FN MO(L_FN)
 #define FN_LED 63
@@ -51,19 +93,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [L_FN] = LAYOUT(
   KC_GESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  KC_INS,
-  _______,   KC_GRV,  _______,  DB_TOGG,  QK_BOOT,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
-  _______,  _______,  _______,  _______,  _______, TG(L_GAME),  _______,  _______,  _______,  _______,  _______,  _______,             _______,
-  _______,  _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,   RGB_VAI,  KC_HOME,
-  _______,  _______,  _______,                                RESET,                                  _______,  _______,  RGB_RMOD,  RGB_VAD,  RGB_MOD),
+  _______,   KC_GRV,  _______,  DB_TOGG,  QK_BOOT,  _______,  _______,  KC_LRGB_RST,  KC_LRGB_TOG,  RGB_TOG,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
+  _______,  _______,  _______,  _______,  _______, TG(L_GAME), _______,  _______,  _______,  _______,  _______,  _______,             _______,
+  _______,  _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  KC_LRGB_VAI,  KC_HOME,
+  _______,  _______,  _______,                                RESET,                                  _______,  _______,  _______,  KC_LRGB_VAD,  _______),
 };
 
 void keyboard_post_init_user(void) {
     // initialize matrix state
-    rgb_matrix_state_update(layer_state);	
+    lrgb_reset();
+    rgb_matrix_refresh(layer_state);	
+}
+
+static void lrgb_reset(void) {
+    // Basically memcpy but too lazy to #include it
+    for (uint8_t i = L_BASE; i < L_NUM_; i++)
+        lrgb_state[i] = LRGB_DEFAULT[i];
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
-    rgb_matrix_state_update(state);
+    rgb_matrix_refresh(state);
     return state;
 }
 
@@ -78,15 +127,52 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
 }
 
-void rgb_matrix_state_update(uint32_t state) {
-    // NOTE: base layer is always on
-    if (IS_LAYER_ON_STATE(state, L_GAME)) {
-	rgb_matrix_sethsv(0, 255, 255);
-        rgb_matrix_mode(RGB_MATRIX_CYCLE_OUT_IN);
-        rgb_matrix_set_speed(48);
-    } else { // base layer
-	rgb_matrix_sethsv(0, 255, 200);
-        rgb_matrix_mode(RGB_MATRIX_CYCLE_ALL);
-        rgb_matrix_set_speed(14);
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        uint8_t l = get_highest_rgb_layer(layer_state);
+        switch (keycode) {
+            // rgb keycodes
+            case KC_LRGB_TOG:
+                lrgb_state[l].on = !lrgb_state[l].on;
+                goto kc_lrgb_exit;
+            case KC_LRGB_RST:
+                lrgb_reset();
+                goto kc_lrgb_exit;
+            case KC_LRGB_VAI:
+                lrgb_state[l].v = clamp(lrgb_state[l].v, 0+LRGB_VA_STEP, 255-LRGB_VA_STEP) + LRGB_VA_STEP;
+                goto kc_lrgb_exit;
+            case KC_LRGB_VAD:
+                lrgb_state[l].v = clamp(lrgb_state[l].v, 0+LRGB_VA_STEP, 255-LRGB_VA_STEP) - LRGB_VA_STEP;
+                goto kc_lrgb_exit;
+            default:
+                return true;
+        }
     }
+    return true;
+
+kc_lrgb_exit:
+    rgb_matrix_refresh(layer_state);
+    return true;
+}
+
+static void rgb_matrix_refresh(uint32_t state) {
+    uint8_t layer = get_highest_rgb_layer(state);
+    struct rgb_state rgbs = lrgb_state[layer];
+    if (rgbs.on) {
+        rgb_matrix_sethsv(rgbs.h, rgbs.s, rgbs.on ? rgbs.v : 0);
+        rgb_matrix_set_speed(rgbs.spd);
+        rgb_matrix_mode(rgbs.mode);
+        return;
+    }
+    rgb_matrix_sethsv(0, 0, 0);
+    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+}
+
+static uint8_t get_highest_rgb_layer(uint32_t state) {
+    // Get highest non-NONE layer. Default layer is always non-NONE and on.
+    for (uint8_t i = L_NUM_-1; i > L_BASE; i--) {
+        if (IS_LAYER_ON_STATE(state, i) && lrgb_state[i].mode != RGB_MATRIX_NONE)
+            return i;
+    }
+    return L_BASE;
 }
